fced8b19afcd1030e632fa1c408537bc1d383441
hwangshyun <hwamg123@naver.com>
2024-04-29 14:16:59 +0900

    주석삭제

--- homework.js
+++ homework.js
@@ -30,7 +30,6 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
                     <p id="grade">${vote}</p>
                 </div>
             `
-        //    console.log(movie_list)
         
         
         document.getElementById('card-list').innerHTML = temp_html
@@ -59,80 +58,3 @@ searchinput.addEventListener('input',function () {
 
  }); 
  
-  
-
-    // let movie_title = Response['results']
-
-    // console.log(movie_title)
-
-    // const filteredMovies = movie_title.fliter((moviename)=> moviename.includes('god'));
-    // console.log(filteredMovies)
-    
-   
-
-
-
-
-
-
-    //   let movie_list = response['results'];
-
-    // //   let temp_html = ``;
-    //   movie_list.forEach(i => {
-    //     let img_url = 'https://image.tmdb.org/t/p/w500' + i['backdrop_path'];
-    //     let movie_title = i['title'];
-    //     let overview = i['overview'];
-    //     let vote = i['vote_average'];
-    //     let id = i['id'];
-  
-    //     temp_html += `
-    //     <div class="col">
-    //       <a href='movie_detail.html?id=${id}'>
-    //         <img src='${img_url}' alt="">
-    //        <div class="info">
-    //         <div class= wrap>
-    //          <h3>${movie_title}</h3>
-    //           <p>${overview}</p>
-    //           <p>⭐ ${vote}</p>
-    //           </div>
-    //       </div>
-    //      </a>
-    //   </div>`;
-  
-    //     document.getElementById('cards-box').innerHTML = temp_html;
-    //   });
-  
-    
-
-
-
-
-   
-
-
-
-
-
-    
-
-    // let rows = data['results'];
-    // const cardList = document.querySelector('.card-list');
-    // cardList.innerHTML = '';
-
-    // rows.forEach((a) => {
-    //     let _title = a['title'];
-    //     let _overview = a['overview'];
-    //     let _poster_path = a['poster_path'];
-    //     let _vote_average = a['vote_average'];
-    //     let _id = a['id'];
-
-    //     let temp_html = `
-    //     <div id="card" >
-    //     <img id="${movie.poster_path}" src=""${movie.poster_path} alt="영화이미지">
-    //     <h3 id="movie-title">${movie.title}</h3>
-    //     <p id="overview">${movie.overview}</p>
-    //     <p id="grade">${movie.vote_average}</p>
-    //     </div>
-    //      `;   
-    //     cardList.insertAdjacentHTML('card-list', temp_html);
-    // });
