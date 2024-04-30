c757bc8ab8e28e4bf526cd045a3c1c6d0a096d67
hwangshyun <hwamg123@naver.com>
2024-04-30 18:01:05 +0900

    grid 적용

--- homework.js
+++ homework.js
@@ -1,60 +1,51 @@
 const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzMxN2Q4ZjQ1ODg2OTMwMjU0Y2NkMDYyZTBlZDhhMSIsInN1YiI6IjY2MmEwODc5ZDE4YjI0MDA5YmRlMDdhOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3Cg1X24o5jVGH9ejSs7O2EBWDSpAb6SHIcyBU_UXq3g'
-    }
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzMxN2Q4ZjQ1ODg2OTMwMjU0Y2NkMDYyZTBlZDhhMSIsInN1YiI6IjY2MmEwODc5ZDE4YjI0MDA5YmRlMDdhOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3Cg1X24o5jVGH9ejSs7O2EBWDSpAb6SHIcyBU_UXq3g"
+  }
 };
- let map =  new Map()
- 
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-    .then(response => response.json())
-    .then(response => {
-        
-        let movie_list = response['results'];
+let map = new Map();
 
-        let temp_html = ``;
-        movie_list.forEach((data)=>{
-           
-            let img_url = 'https://image.tmdb.org/t/p/w500' + data['backdrop_path'];
-            let movie_title = data['title'];
-            let overview = data['overview'];
-            let vote = data['vote_average'];
-            let movieid = data['id'];
+fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", options)
+  .then((response) => response.json())
+  .then((response) => {
+    let movie_list = response["results"];
 
-            temp_html += `
+    let temp_html = ``;
+    movie_list.forEach((data) => {
+      let img_url = "https://image.tmdb.org/t/p/w500" + data["backdrop_path"];
+      let movie_title = data["title"];
+      let overview = data["overview"];
+      let vote = data["vote_average"];
+      let movieid = data["id"];
+
+      temp_html += `
                     <div id="card" onclick="alert(${movieid})">
                     <img id="image" src="${img_url}" alt="영화이미지">
                     <h3 id="movie-title" class = "movietitle">${movie_title}</h3>
                     <p id="overview">${overview}</p>
                     <p id="grade">${vote}</p>
                 </div>
-            `
-        
-        
-        document.getElementById('card-list').innerHTML = temp_html
-        
-        
-       
-})
+            `;
+      document.getElementById("card-list").innerHTML = temp_html;
+    });
 
-const searchinput = document.getElementById('movie_name_input')
-const searchBtn = document.getElementById('searchbtn')
+    const searchinput = document.getElementById("movie_name_input");
+    const searchBtn = document.getElementById("searchbtn");
 
-searchinput.addEventListener('input',function () {
-    const searchTerm = searchinput.value.toLowerCase();
-    const movieList = document.querySelectorAll('#card');
+    searchinput.addEventListener("input", function () {
+      const searchTerm = searchinput.value.toLowerCase();
+      const movieList = document.querySelectorAll("#card");
 
-    movieList.forEach ((card)=>{
-        const title = card.querySelector('.movietitle').textContent.toLowerCase();
+      movieList.forEach((card) => {
+        const title = card.querySelector(".movietitle").textContent.toLowerCase();
         if (title.includes(searchTerm)) {
-            card.style.display = 'block';
+          card.style.display = "block";
         } else {
-            card.style.display = 'none'
+          card.style.display = "none";
         }
-    })
-}     
-)
-
- }); 
- 
+      });
+    });
+  });
